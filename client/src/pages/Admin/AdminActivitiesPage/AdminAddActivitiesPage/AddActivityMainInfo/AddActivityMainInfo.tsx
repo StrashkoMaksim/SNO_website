57c9e2cf22c7dfa74932438bef27fec147b1dff7
fire-insurance <78022631+fire-insurance@users.noreply.github.com>
2022-03-04 23:25:43 +1000

    AdminAddActivitiesPage decomposition

--- client/src/pages/Admin/AdminActivitiesPage/AdminAddActivitiesPage/AddActivityMainInfo/AddActivityMainInfo.tsx
+++ client/src/pages/Admin/AdminActivitiesPage/AdminAddActivitiesPage/AddActivityMainInfo/AddActivityMainInfo.tsx
@@ -1,6 +1,85 @@
+import styles from './AddActivityMainInfo.module.scss'
+import { useRef, useCallback, FormEvent, ComponentRef, useState } from 'react'
+import cn from 'classnames';
+import Activity from '../../../../../components/Activities/Activity/Activity';
+import AdminFormInputText, { AFITStyle } from '../../../../../components/Admin/AdminFormInputText/AdminFormInputText';
+import AdminFormInputImg from '../../../../../components/Admin/AdminFormInputImg/AdminFormInputImg';
+import Editor from '../../../../../components/Editor/Editor';
+import placeholderImg from '../../../../../assets/img/roundPlaceholderImg.png'
+
+const emptyActivityMainInfo = {
+    name: '',
+    previewText: '',
+    logo: '',
+    content: '',
+}
+
+interface ActivityMainInfo {
+    name: string,
+    previewText: string,
+    logo: File | Blob | string,
+    content: string,
+}
+
 const AddActivityMainInfo = () => {
+
+    const [activityMainInfo, setActivityMainInfo] = useState<ActivityMainInfo>(emptyActivityMainInfo)
+
+    const onChangeTextInputsHandle = (e: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+
+        const inputName = e.currentTarget.name;
+        const inputValue = e.currentTarget.value;
+
+        setActivityMainInfo(prevState => ({ ...prevState, [inputName]: inputValue }))
+    }
+
+    const onPreviewImgLoad = (event: any) => {
+        const img = event.target.files[0];
+        setActivityMainInfo(prevState => ({ ...prevState, logo: img }))
+    }
+
+    const editorCore = useRef<ComponentRef<any>>(null)
+
+    const editorInitializeHandler = useCallback((instance) => {
+        editorCore.current = instance
+    }, [])
+
+
     return (
-        <></>
+        <div className={cn(styles['admin-add-form'], styles.form)}>
+            <div className={styles.mainInputs}>
+                <div className={styles.mainInputs__TextInputs}>
+                    <AdminFormInputText
+                        style={AFITStyle.textarea}
+                        placeholder="Заголовок"
+                        name="name"
+                        value={activityMainInfo.name}
+                        onChange={onChangeTextInputsHandle}
+                        extraClass={styles['input-name']}
+                        required
+                    />
+
+                    <AdminFormInputText
+                        style={AFITStyle.textarea}
+                        placeholder="Описание"
+                        name="previewText"
+                        value={activityMainInfo.previewText}
+                        onChange={onChangeTextInputsHandle}
+                        extraClass={styles['input-previewText']}
+                        required
+                    />
+                </div>
+
+                <AdminFormInputImg
+                    name="previewImg"
+                    onChange={onPreviewImgLoad}
+                    defaultImg={placeholderImg}
+                    id='activityLogoInputImg'
+                    extraClass={styles.logo}
+                />
+            </div>
+            <Editor onInitialize={editorInitializeHandler} />
+        </div>
     )
 }
 
--- client/src/pages/Admin/AdminActivitiesPage/AdminAddActivitiesPage/AdminAddActivitiesPage.tsx
+++ client/src/pages/Admin/AdminActivitiesPage/AdminAddActivitiesPage/AdminAddActivitiesPage.tsx
@@ -1,46 +1,15 @@
 import DefaultButton, { ButtonStyles, ButtonTypes } from "../../../../components/DefaultButton/DefaultButton";
 import styles from './AdminAddActivitiesPage.module.scss'
-import { ComponentRef, FormEvent, useState } from "react";
-import AdminFormInputText, { AFITStyle } from "../../../../components/Admin/AdminFormInputText/AdminFormInputText";
-import AdminFormInputImg from "../../../../components/Admin/AdminFormInputImg/AdminFormInputImg";
-import placeholderImg from '../../../../assets/img/roundPlaceholderImg.png'
-import cn from "classnames";
-import Editor, { getEditorContent } from "../../../../components/Editor/Editor";
-import React from "react";
+import { FormEvent, useState } from "react";
+
 import AdminAddSupervisorForm from "../../../../components/Admin/AdminAddSupervisorForm/AdminAddSupervisorForm";
 import SchedulePicker from "./SchedulePicker/SchedulePicker";
 import { ScheduleIntefrace } from '../../../../types/schedule'
 import AdminEditPageHeader, { AEPHTypes } from "../../../../components/AdminEditPageHeader/AdminEditPageHeader";
+import AddActivityMainInfo from "./AddActivityMainInfo/AddActivityMainInfo";
+import { Activity, emptyActivity } from "../../../../types/activity";
 
-interface Activity {
-    name: string,
-    previewText: string,
-    logo: string | Blob | File,
-    supervisor: {
-        fio: string,
-        department: string,
-        position: string,
-        phone: string
-    }
-    supervisorPhoto: string | Blob | File,
-    content: string,
-    schedule: ScheduleIntefrace[]
-}
 
-const emptyActivity = {
-    name: '',
-    previewText: '',
-    logo: '',
-    supervisor: {
-        fio: '',
-        department: '',
-        position: '',
-        phone: ''
-    },
-    supervisorPhoto: '',
-    content: '',
-    schedule: []
-}
 
 const AdminAddActivitiesPage = () => {
 
@@ -51,29 +20,9 @@ const AdminAddActivitiesPage = () => {
         e.preventDefault()
 
         // const formData = new FormData(e.currentTarget)
-
         // const editorData = await getEditorContent(editorCore);
-
     }
 
-    const onChangeTextInputsHandle = (e: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-
-        const inputName = e.currentTarget.name;
-        const inputValue = e.currentTarget.value;
-
-        setActivity(prevState => ({ ...prevState, [inputName]: inputValue }))
-    }
-
-    const onPreviewImgLoad = (event: any) => {
-        const img = event.target.files[0];
-        setActivity(prevState => ({ ...prevState, logo: img }))
-    }
-
-    const editorCore = React.useRef<ComponentRef<any>>(null)
-
-    const editorInitializeHandler = React.useCallback((instance) => {
-        editorCore.current = instance
-    }, [])
 
     return (
         <>
@@ -86,41 +35,7 @@ const AdminAddActivitiesPage = () => {
 
             <form className={styles.AddActivityForm} onSubmit={submitHandler}>
 
-                {/* По хорошему переименовать класс */}
-                <div className={cn(styles['admin-add-form'], styles.form)}>
-                    <div className={styles.mainInputs}>
-                        <div className={styles.mainInputs__TextInputs}>
-                            <AdminFormInputText
-                                style={AFITStyle.textarea}
-                                placeholder="Заголовок"
-                                name="name"
-                                value={activity.name}
-                                onChange={onChangeTextInputsHandle}
-                                extraClass={styles['input-name']}
-                                required
-                            />
-
-                            <AdminFormInputText
-                                style={AFITStyle.textarea}
-                                placeholder="Описание"
-                                name="previewText"
-                                value={activity.previewText}
-                                onChange={onChangeTextInputsHandle}
-                                extraClass={styles['input-previewText']}
-                                required
-                            />
-                        </div>
-
-                        <AdminFormInputImg
-                            name="previewImg"
-                            onChange={onPreviewImgLoad}
-                            defaultImg={placeholderImg}
-                            id='activityLogoInputImg'
-                            extraClass={styles.logo}
-                        />
-                    </div>
-                    <Editor onInitialize={editorInitializeHandler} />
-                </div>
+                <AddActivityMainInfo />
 
                 <AdminAddSupervisorForm />
 
