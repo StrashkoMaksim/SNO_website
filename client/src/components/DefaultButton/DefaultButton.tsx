fa4d4a213b62c4349aaa427f85575ec4056a52fc
Strashko Maksim <maksim000072@gmail.com>
2022-03-13 12:27:47 +1000

    EditPage adaptive

--- client/src/components/DefaultButton/DefaultButton.tsx
+++ client/src/components/DefaultButton/DefaultButton.tsx
@@ -1,8 +1,7 @@
 import styles from "./DefaultButton.module.scss"
-import { MouseEventHandler } from "react";
-import cn from "classnames";
-
-import React, { FC } from "react";
+import { MouseEventHandler } from "react"
+import cn from "classnames"
+import React, { FC } from "react"
 
 export enum ButtonStyles {
     filled = "filled",
@@ -17,12 +16,12 @@ export enum ButtonTypes {
 }
 
 interface ButtonProps {
-    text: string,
-    style: ButtonStyles,
-    type: ButtonTypes,
-    imgSrc?: string,
-    onClick?: MouseEventHandler<HTMLButtonElement>,
-    extraClass?: string;
+    text: string
+    style: ButtonStyles
+    type: ButtonTypes
+    imgSrc?: string
+    onClick?: MouseEventHandler<HTMLButtonElement>
+    extraClass?: string
 }
 
 const DefaultButton: FC<ButtonProps> = ({ text, style, type, imgSrc, onClick, extraClass }) => {
@@ -39,4 +38,4 @@ const DefaultButton: FC<ButtonProps> = ({ text, style, type, imgSrc, onClick, ex
     )
 }
 
-export default DefaultButton;
+export default DefaultButton
